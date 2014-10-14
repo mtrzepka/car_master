3b44de3b812b1565b98a3d36fd3a22453919e5d7
Michael Rzepka <mike.rzepka@cerner.com>
2014-10-13 22:00:14 -0500

    Add Car Value Activity

--- Car_Master_App/gen/com/example/carmaster/R.java
+++ Car_Master_App/gen/com/example/carmaster/R.java
@@ -24,38 +24,44 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Date_label=0x7f080016;
-        public static final int Link_label=0x7f080017;
-        public static final int Submit=0x7f080009;
-        public static final int action_settings=0x7f080018;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080004;
-        public static final int button4=0x7f080003;
-        public static final int button5=0x7f080005;
-        public static final int cDescription=0x7f08000c;
-        public static final int cdescription_label=0x7f080014;
-        public static final int dDescription=0x7f08000b;
-        public static final int ddescription_label=0x7f080013;
-        public static final int link=0x7f08000e;
-        public static final int manufacturer=0x7f08000f;
-        public static final int mf_label=0x7f080012;
-        public static final int note_label=0x7f080015;
-        public static final int notes=0x7f08000d;
-        public static final int pubDate=0x7f080010;
-        public static final int spinner1=0x7f080006;
-        public static final int spinner2=0x7f080007;
-        public static final int spinner3=0x7f080008;
-        public static final int textView1=0x7f080000;
-        public static final int title=0x7f08000a;
-        public static final int title_label=0x7f080011;
+        public static final int Date_label=0x7f08001b;
+        public static final int Link_label=0x7f08001c;
+        public static final int Submit=0x7f08000e;
+        public static final int action_settings=0x7f08001d;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080007;
+        public static final int button3=0x7f080009;
+        public static final int button4=0x7f080008;
+        public static final int button5=0x7f08000a;
+        public static final int cDescription=0x7f080011;
+        public static final int cdescription_label=0x7f080019;
+        public static final int dDescription=0x7f080010;
+        public static final int ddescription_label=0x7f080018;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080003;
+        public static final int editText3=0x7f080000;
+        public static final int editText4=0x7f080002;
+        public static final int editText5=0x7f080005;
+        public static final int link=0x7f080013;
+        public static final int manufacturer=0x7f080014;
+        public static final int mf_label=0x7f080017;
+        public static final int note_label=0x7f08001a;
+        public static final int notes=0x7f080012;
+        public static final int pubDate=0x7f080015;
+        public static final int spinner1=0x7f08000b;
+        public static final int spinner2=0x7f08000c;
+        public static final int spinner3=0x7f08000d;
+        public static final int textView1=0x7f080006;
+        public static final int title=0x7f08000f;
+        public static final int title_label=0x7f080016;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int list_view=0x7f030001;
-        public static final int recall_activity_main=0x7f030002;
-        public static final int recall_list_view=0x7f030003;
-        public static final int single_recall_detail=0x7f030004;
+        public static final int activity_car_value=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int list_view=0x7f030002;
+        public static final int recall_activity_main=0x7f030003;
+        public static final int recall_list_view=0x7f030004;
+        public static final int single_recall_detail=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -63,8 +69,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050006;
         public static final int made_prompt=0x7f050002;
         public static final int model_prompt=0x7f050003;
+        public static final int title_activity_car_value=0x7f050005;
         public static final int year_prompt=0x7f050004;
     }
     public static final class style {
